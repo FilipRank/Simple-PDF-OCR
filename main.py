9dcaeaed5e3e113f27b701eb1265398cb49ef531
filip <filiprankovic22@gmail.com>
2026-06-02 13:16:53 +0200

    add code

---
+++ main.py
@@ -0,0 +1,86 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import os
+import threading
+import ocrmypdf
+
+def worker(input_files, output_dir):
+    success_count = 0
+
+    for pdf in input_files:
+        filename = os.path.basename(pdf)
+        output_pdf = os.path.join(
+            output_dir,
+            filename
+        )
+
+        try:
+            status_label.config(
+                text=f"Processing: {filename}"
+            )
+
+
+            ocrmypdf.ocr(
+                input_file_or_options=pdf,
+                output_file=output_pdf,
+                deskew=True,
+                clean=True,
+                force_ocr=True,
+                rotate_pages=True
+            )
+
+            success_count += 1
+        except Exception as e:
+            print(f"Failed on file: {filename}")
+            print(e)
+        
+    status_label.config(
+        text=f"Finished {success_count}/{len(input_files)} files"
+    )
+
+    messagebox.showinfo(
+        "Complete",
+        f"Processed {success_count} of {len(input_files)} files."
+    )
+
+
+def run_ocr():
+    input_files = filedialog.askopenfilenames(
+        title="Select PDFs",
+        filetypes=[("PDF Files", "*.pdf")]
+    )
+
+    if not input_files:
+        return
+    
+    output_dir = filedialog.askdirectory(
+        title="Select output folder"
+    )
+
+    if not output_dir:
+        return
+    
+    threading.Thread(
+        target=worker,
+        args=(input_files, output_dir),
+        daemon=True
+    ).start()
+
+root = tk.Tk()
+root.title("Simple PDF OCR")
+root.geometry("400x200")
+
+tk.Button(
+    root,
+    text="Select PDF and OCR",
+    command=run_ocr
+).pack(pady=40)
+
+status_label = tk.Label(
+    root,
+    text="Ready"
+)
+status_label.pack()
+
+
+root.mainloop()
